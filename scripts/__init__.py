ccc9efefe64c2beed84d5914f56109bd1f003a6c
timur <timurka.zavyalov@mail.ru>
2023-02-27 17:23:50 +0500

    git init

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -154,5 +154,7 @@ LIST_OF_FLOAT_TYPE: list = ["weight"]
 LIST_OF_INT_TYPE: list = ["wagon_number", "cargo_code_of_the_etsng", "shipper_okpo", "departure_station_code_of_rf",
                           "consignee_okpo", "destination_station_code_of_rf", "carriage_fee", "cargo_class",
                           "departure_station_code_of_cis", "destination_station_code_of_cis",
-                          "code_of_conditional_type_of_wagon"]
+                          "code_of_conditional_type_of_wagon", "wagon_kilometers",
+                          "cargo_code_according_to_the_harmonized_nomenclature_of_goods_gng", "quantity_of_wagons",
+                          "quantity_of_containers", "transportation_volume_tons"]
 LIST_OF_DATE_TYPE: list = ["departure_date"]
